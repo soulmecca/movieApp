cee77d1319cd59d25e1387ad156574679f08b3ed
savit <thesavit@gmail.com>
2018-12-17 20:12:41 +0900

    added onClear function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -7,13 +9,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   myInput: string;
+  favorites: Observable<any>;
+  data;
 
-  constructor(public navCtrl: NavController) {
+  	constructor(
+		public navCtrl: NavController, 
+		public httpClient: HttpClient
+		) {}
 
-  }
+  	onInput (input) {
+		// this.favorites = this.httpClient.get('http://localhost:3000/favorites.json');
+		// this.favorites.subscribe(data => {
+		// 	console.log('$$$$$', data)
+		// })
+		if(this.myInput === '') return 
+		this.favorites = this.httpClient.get(`http://localhost:3000/favorites/find/${this.myInput}.json` );
+		this.favorites.subscribe(data => {
+			console.log('$$$$$', data)
+			if(data) {
+				this.data = data;
+			}
+		}, err=> {
+			console.log('error is ', err)
+		})
 
-  onInput (input) {
-        
-  }
+		// const body = {
+		// 	title: "superman",
+		// 	ranking: 10,
+		// 	comment: 'very good'
+		// }
+		// this.httpClient.post('http://localhost:3000/favorites.json', body)
+		// .subscribe(res => {
+		// 	console.log('#### ', res)
+		// }, err => {
+		// 	console.log('error is ', err);
+		// })
+		
+	  }
+	  
+	onClear  () {
+		console.log('called!!!')
+	}
 
 }
